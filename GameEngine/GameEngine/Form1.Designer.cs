cde92a92d9a1f379dd5c2860ef92c86c88be5037
skororassvet <supboi@protonmail.com>
2022-11-16 01:29:34 +0000

    Basic prototype

---
+++ GameEngine/GameEngine/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace GameEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.image1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Scripting = new System.Windows.Forms.Button();
+            this.scene_button = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Up = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // image1
+            // 
+            this.image1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.image1.Location = new System.Drawing.Point(100, 25);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(300, 300);
+            this.image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image1.TabIndex = 1;
+            this.image1.TabStop = false;
+            this.image1.Click += new System.EventHandler(this.image1_Click);
+            this.image1.DoubleClick += new System.EventHandler(this.image1_DoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.Scripting);
+            this.panel1.Controls.Add(this.scene_button);
+            this.panel1.Location = new System.Drawing.Point(-9, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1163, 61);
+            this.panel1.TabIndex = 2;
+            // 
+            // Scripting
+            // 
+            this.Scripting.Location = new System.Drawing.Point(133, 16);
+            this.Scripting.Name = "Scripting";
+            this.Scripting.Size = new System.Drawing.Size(94, 29);
+            this.Scripting.TabIndex = 1;
+            this.Scripting.Text = "Scripting";
+            this.Scripting.UseVisualStyleBackColor = true;
+            // 
+            // scene_button
+            // 
+            this.scene_button.Location = new System.Drawing.Point(24, 16);
+            this.scene_button.Name = "scene_button";
+            this.scene_button.Size = new System.Drawing.Size(94, 29);
+            this.scene_button.TabIndex = 0;
+            this.scene_button.Text = "Scene";
+            this.scene_button.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.image1);
+            this.panel2.Location = new System.Drawing.Point(12, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(597, 361);
+            this.panel2.TabIndex = 3;
+            this.panel2.DoubleClick += new System.EventHandler(this.panel2_DoubleClick);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(622, 77);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(230, 361);
+            this.panel3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(15, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "File";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.Controls.Add(this.delete_button);
+            this.panel4.Location = new System.Drawing.Point(12, 444);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1085, 66);
+            this.panel4.TabIndex = 5;
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(3, 3);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(142, 60);
+            this.delete_button.TabIndex = 0;
+            this.delete_button.Text = "Delete object";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel5.Controls.Add(this.textBox3);
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Controls.Add(this.trackBar1);
+            this.panel5.Location = new System.Drawing.Point(867, 77);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(230, 361);
+            this.panel5.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(20, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 27);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Resize";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(20, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Edit";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(86, 39);
+            this.trackBar1.Maximum = 300;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(130, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Up
+            // 
+            this.Up.Tick += new System.EventHandler(this.Up_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1105, 522);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox image1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private TrackBar trackBar1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private Button delete_button;
+        private Button Scripting;
+        private Button scene_button;
+        private System.Windows.Forms.Timer Up;
+    }
+}
---
+++ GameEngine/GameEngine/Form1.cs
@@ -0,0 +1,107 @@
+using System.Diagnostics;
+
+namespace GameEngine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            movingObject_checkbox.Checked = true;
+            trackBar1.Value = 300;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+
+                    image1.ImageLocation = imageLocation;
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Error","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            image1.Size = new Size(trackBar1.Value, image1.Size.Height);
+            image1.Left = (ClientSize.Width - image1.Width) / 2;
+            image1.Left = (ClientSize.Height - image1.Height) / 2;
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (image1.ImageLocation != "")
+                {
+                    image1.ImageLocation = "";
+                }
+                else
+                {
+                    MessageBox.Show("Error", "Image does not exist", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void movingObject_checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        int newX = 0;
+        int newY = 0;
+
+        private void Up_Tick(object sender, EventArgs e)
+        {
+            newX = Cursor.Position.X - 250;
+            newY = Cursor.Position.Y - 350;
+            image1.Location = new Point(newX, newY);
+        }
+
+
+        private void image1_Click(object sender, EventArgs e)
+        {
+            Up.Enabled = true;
+
+        }
+
+        private void image1_DoubleClick(object sender, EventArgs e)
+        {
+            Up.Enabled = false;
+        }
+
+        private void panel2_DoubleClick(object sender, EventArgs e)
+        {
+            Up.Enabled = false;
+        }
+    }
+}
